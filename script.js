c7465c13040e5c35ae910884191f5b226dbd2166
Gustavo <gustavoxnogueira726@gmail.com>
2023-01-23 13:27:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia Ja incluso")
+    return
+  }
+  
+  nlwSetup.addDay(today)
+}
+function save() {
+  localStorage.setItem("save@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("save@habits")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
